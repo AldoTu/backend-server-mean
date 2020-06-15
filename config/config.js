b455a0303bd9a67abd75e317fb7229fee677d39c
Aldo Turmon <aldoturegano10@hotmail.com>
2020-06-15 01:22:37 -0500

    Sección Google terminada - backend terminado

--- config/config.js
+++ config/config.js
@@ -1,1 +1,4 @@
 module.exports.SEED = 'este-es-un-seed-dificil';
+
+// Google
+module.exports.CLIENT_ID = '540307093378-a729brk904tb78akmm3umk562jbvqvvi.apps.googleusercontent.com';
--- models/usuario.js
+++ models/usuario.js
@@ -12,7 +12,8 @@ var usuarioSchema = new Schema({
     email: { type: String, unique: true, required: [true, 'El correo es necesario'] },
     password: { type: String, required: [true, 'La contraseña es necesaria'] },
     img: { type: String },
-    role: { type: String, required: true, default: 'USER_ROLE', enum: rolesValidos }
+    role: { type: String, required: true, default: 'USER_ROLE', enum: rolesValidos },
+    google: { type: Boolean, default: false }
 });
 
 usuarioSchema.plugin(uniqueValidator, { message: '{PATH} debe ser único' });
